f07a5c452c31111ef4c0ecc5e14b6b2b10584acf
richusherseen <richusherseen786@gmail.com>
2020-12-24 11:53:07 +0530

    fetching cities from database and displying it

--- weather/views.py
+++ weather/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render,render
 import requests
+from weather.models import City
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=314b03e761de15a2e6fc1e7822396b7e'#api link with api key
     city = 'Las Vegas'
-
-    r = requests.get(url.format(city)).json()#taking the information from api inform of json
     
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
 
-    city_weather = {
+        r = requests.get(url.format(city)).json()#taking the information from api inform of json
+        
+        
+        city_weather = {
 
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'descripiton' : r['weather'][0]['description'],
-        'icon' :  r['weather'][0]['icon']
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'descripiton' : r['weather'][0]['description'],
+            'icon' :  r['weather'][0]['icon']
 
-    }
-    context = {'city_weather' : city_weather}
+        }
+        weather_data.append(city_weather)
+       
+    context = {'weather_data' : weather_data}
 
     return render(request,'weather.html',context)
