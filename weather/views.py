fa2b4e30108eb6137e38baa8aca0f69ac65153f9
richusherseen <richusherseen786@gmail.com>
2020-12-24 12:42:31 +0530

    weather app compeleted

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render,render
 import requests
 from weather.models import City
+from .forms import CityForm
+from django.contrib import messages
+
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=314b03e761de15a2e6fc1e7822396b7e'#api link with api key
     city = 'Las Vegas'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            city_count = City.objects.filter(name = new_city).count()
+
+            if city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    messages.warning(request,f'City dosnt exists')
+            else:
+                
+                messages.warning(request,f'city already added')
     
+    form = CityForm()
+
     cities = City.objects.all()
     weather_data = []
     for city in cities:
@@ -22,6 +43,6 @@ def index(request):
         }
         weather_data.append(city_weather)
        
-    context = {'weather_data' : weather_data}
+    context = {'weather_data' : weather_data,'form' : form}
 
     return render(request,'weather.html',context)
